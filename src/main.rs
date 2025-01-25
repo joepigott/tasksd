f6111000d08906214dea17029ad930d6230e7b3d
Joe Pigott <joe.pigs@protonmail.com>
2025-01-25 14:39:42 -0700

    added basic scheduler/server init, also task data recovery at startup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use piglog;
+use taskscheduler::vars;
+use taskscheduler::TaskQueue;
+use taskscheduler::scheduler::Scheduler;
+use taskscheduler::server::Server;
+use std::fs;
+use std::sync::{Arc, Mutex};
+
+#[tokio::main]
+async fn main() {
+    let storage = match vars::storage_path() {
+        Ok(s) => s,
+        Err(e) => {
+            piglog::error!("{e}");
+            return;
+        }
+    };
+
+    let queue = Arc::new(Mutex::new(recover_queue(&storage).unwrap_or(TaskQueue::new())));
+    let scheduler = Scheduler::with_queue(Arc::clone(&queue));
+    let server = Server::with_queue(Arc::clone(&queue));
+}
+
+fn recover_queue(storage: &str) -> Option<TaskQueue> {
+    let data = fs::read_to_string(storage).ok()?;
+    serde_json::from_str::<TaskQueue>(&data).ok()
 }
